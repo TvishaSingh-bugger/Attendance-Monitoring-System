2e772a29d8295e713d69aaf9b071a6c3d9c15a59
TvishaSingh-bugger <tvishasingh15@gmail.com>
2025-10-25 17:13:39 +0530

    Update mainlogin.c

--- mainlogin.c
+++ mainlogin.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "authentication.c"
-#include "teachers.c"
-#include "student.c"
-#include "attendanceTeacher.c"
-#include "attendanceStudent.c"
+#include "attendance.h"
+#include "authentication.h"
 
 int main()
 {
   int choice;
-  struct Node *teacherhead=NULL;
-  struct node *studenthead=NULL;
-
   while (1)
   {
     printf("ATTENDANCE SYSTEM\n");
@@ -33,15 +27,13 @@ int main()
       char category[30];
       printf("Enter your category (Teacher/Student): ");
       scanf("%s", category);
-      if (strcmp(category, "Teacher") == 0 || strcmp(category, "teacher") == 0)
+      if(strcmp(category, "Teacher")==0)
       {
-        displayMessageTeacher();
-        teachersChoice(teacherhead);
+        teachersChoice();
       }
-      else if (strcmp(category, "Student") == 0 || strcmp(category, "student") == 0)
+      else if(strcmp(category, "Student")==0)
       {
-        displayMessageStudent();
-        studentChoice(studenthead);
+        studentChoice();
       }
       else
       {
@@ -51,7 +43,7 @@ int main()
     break;
     case 3:
       printf("Exiting the Program...\n");
-      return;
+      return 0;
     default:
       printf("Invalid Choice!\n");
       break;
