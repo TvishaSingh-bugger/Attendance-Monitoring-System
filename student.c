22dad381f356457f5258d8e542965dffcbe9cd88
TvishaSingh-bugger <tvishasingh15@gmail.com>
2025-10-25 17:16:18 +0530

    Update student.c

--- student.c
+++ student.c
@@ -1,40 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "authentication.c"
-#include "teachers.c"
-#include "attendanceTeacher.c"
-#include "attendanceStudent.c"
+#include "attendance.h"
+#include "authentication.h"
 
-void displayMessageStudent()
-{
-  displayName();
-}
 
 // Student Dashboard
 
-int studentChoice(struct node *studenthead)
+
+int studentChoice()
 {
   int choice;
   while (1)
   {
-    printf("1:Scan QR\n");
-    printf("2:View My Attendance\n");
-    printf("3:Logout\n");
+    printf("1:View My Attendance\n");
+    printf("2:Logout\n");
     printf("Enter Your Choice:");
     scanf("%d", &choice);
 
     switch (choice)
     {
     case 1:
-      scanQRCode();
+    viewMyAttendance();
       break;
     case 2:
-      viewMyAttendance(studenthead);
-      break;
-    case 3:
-      printf("Logging out..\n");
-      return 0;
+     printf("Logging out..\n");
+     return 0;
     default:
       printf("Invalid Choice!\n");
       break;
